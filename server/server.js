215f2728f2544b2a1cef76c51d739a012d91ef4c
accio-Akshaya <akshayaganesh0910@gmail.com>
2026-04-13 11:52:21 -0400

    fix issue

--- server/server.js
+++ server/server.js
@@ -17,10 +17,10 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 app.use(
-    cors({
-        origin: process.env.CLIENT_URL?.split(',') || 'http://localhost:5173',
-        credentials:true
-    })
+  cors({
+    origin: "https://quiz-app-ten-gamma-77.vercel.app",
+    credentials: true,
+  })
 );
 
 app.use(express.json());
